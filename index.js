3a0dfb04ef92c47a25d8887c3d17851ef5305828
farzad <farzadps44game@gmail.com>
2023-03-21 05:34:49 +0330

    add redis to project

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
+const { createClient } = require("redis");
+
+const client = createClient();
 
 const port = 3000;
 const io = require("socket.io")(server, { cors: { origin: "*" } });
 
 app.set("view engine", "ejs");
-let username = "";
+
+const initialMessages = async (socket) => {
+  client.lrange("message", 0, -1, (err, data) => {
+    data.map((item) => {
+      const [username, message] = item.split(":");
+      socket.emit("message", { username, message });
+    });
+  });
+};
 
 app.get("/chat", (req, res) => {
+  const { username } = req.query;
   res.render("chat", { username: username });
 });
 
@@ -20,14 +32,10 @@ server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-io.on("connection", (socket) => {
-  console.log("user: ", socket.id);
-
-  socket.on("new-user", (data) => {
-    socket.broadcast.emit("update", `new user ${data}`);
-    username = data;
-  });
-  socket.on("message", (data) => {
-    socket.broadcast.emit("message", { data, username });
+io.on("connection", async (socket) => {
+  initialMessages(socket);
+  socket.on("message", async ({ username, message }) => {
+    client.lpush("message", `${username}:${message}`);
+    io.emit("message", { username, message });
   });
 });
